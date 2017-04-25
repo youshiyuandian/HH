7b6010b9c013dcbf965e0082c94f56ff36d55a9e
zuxi li <ios_lizuxi@163.com>
2017-04-25 13:02:54 +0800

    Wan

---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by zuxi li on 2017/4/25.
+//  Copyright © 2017年 zuxi li. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Person.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [Person eat];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
